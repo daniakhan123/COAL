f9fa3d5ab26074ac939b37341bba92f8a65feeda
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2025-07-29 17:59:02 +0500

    Update fibonacci.asm

--- practice/fibonacci.asm
+++ practice/fibonacci.asm
@@ -1,32 +1,42 @@
-INCLUDE Irvine32.inc
-;fibonacci series till 10 numbers
+Include Irvine32.inc
 .data
-
+fibarray dword 50 dup (0)
 .code
 main PROC
-mov ebx , 0
+mov esi , offset fibarray
+
+call fibonacci
+mov esi , offset fibarray
+mov ecx , lengthof fibarray
+l2:
+mov eax , [esi]
+call writedec
+call crlf
+add esi , 4
+loop l2
+
+fibonacci proc
+mov eax , 0
+mov [esi] , eax
+add esi , 4
+mov ebx , 1
+mov eax , ebx
+mov [esi] , eax
+add esi , 4
+mov ecx , 48
 mov edx , 0
-
-mov eax , 0
-call writedec
-call crlf
-mov eax , 1
-call writedec
-call crlf
-
-mov ecx , 8
-mov edx , 1
-mov eax , 0
+mov eax ,0
 l1:
-add eax , edx
-call writedec
-call crlf
-mov ebx , eax
-mov eax ,edx
+add eax , ebx
+mov [esi] , eax
+add esi , 4
 mov edx , ebx
-
+mov ebx , eax
+mov eax , edx
 loop l1
+ret
+fibonacci endp
 
 exit
 main endp
-END main 
+end main
